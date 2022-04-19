f02ec4b978f1b586f9583a3b7e6964da81e5306d
coponico <nicolasjoli@yahoo.com>
2022-04-19 15:52:03 -0300

    navbar v.1.0.1

--- js/script.js
+++
@@ -1,4 +0,0 @@
-function othername() {
-    var input = document.getElementById("userInput").value;
-    console.log(input)
-}
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+<header classname="header">
+    <p>FitnessReact</p>
+    <img src="../../img/react.png" alt=""></img>
+<ul>
+    <li><a href="http://www.google.com">Inicio</a></li>
+    <li><a href="http://www.google.com">xcosa</a></li>
+    <li><a href="http://www.google.com">xotracosa</a></li>
+</ul>
+</header>
+    )
+  }
+  export default NavBar
