bca81ee4060db95dc15cd26429d9c91fdd2ec0ca
coponico <nicolasjoli@yahoo.com>
2022-04-25 17:14:31 -0300

    ItemCount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemlistContainer';
+import { ItemCount } from './components/ItemCount/ItemCount';
 
 function App() {
+  const returnCount = (count) => {
+    alert (count)
+  }
   return (
     <div className="App">
 <NavBar />
-<ItemListContainer greeting='{Mis productos}' />
-    
+    <ItemCount stock={6} initial={1} onAdd={returnCount}/>  
+<ItemListContainer greeting = 'Bienvenidos a mi tienda'/>
     </div>
 
   );
---
+++ src/components/itemcount/ItemCount.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+export const ItemCount = ({ stock, initial, onAdd }) => {
+	const [ count, setCounter ] = useState(initial);
+    const canAdd = count <= stock
+	const suma = () => {
+			setCounter(count + 1);
+	};
+
+	const resta = () => {
+		if (count > 1) {
+			setCounter(count - 1);
+		}
+	};
+	return (
+		<div>
+			<div>
+				<button onClick={resta}>-</button>
+				<h3>{count}</h3>
+				<button onClick={suma}>+</button>
+			</div>
+			<div>
+				<button onClick={() => onAdd(count)} disabled={!canAdd}>Agregar al carrito</button>
+			</div>
+		</div>
+	);
+};
