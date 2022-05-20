02f59b9dc50d199c0e1f4a3de419ee08e6cf6e41
coponico <nicolasjoli@yahoo.com>
2022-05-20 12:24:42 -0300

    ItemCollection I

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { CartContextProvider } from './store/CartContext.jsx';
+import { initializeApp } from "firebase/app";
 
-ReactDOM.render(
+const firebaseConfig = {
+  apiKey: "AIzaSyAfs0l2MWdacmD8kA7sbTeBTrfYnK9wTpE",
+  authDomain: "fitnessreact-fe807.firebaseapp.com",
+  projectId: "fitnessreact-fe807",
+  storageBucket: "fitnessreact-fe807.appspot.com",
+  messagingSenderId: "433762938711",
+  appId: "1:433762938711:web:4f7f105985df646f811812",
+  measurementId: "G-V4JQXDJKHE"
+};
+const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
     <CartContextProvider>
@@ -14,8 +29,11 @@ ReactDOM.render(
       </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/ItemListContainer/ItemlistContainer.jsx
+++ src/pages/ItemListContainer/ItemlistContainer.jsx
@@ -1,71 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemList from "../../components/ItemList/ItemList";
+import { collection, getDocs, getFirestore, query, where, limit } from 'firebase/firestore';
 
 function getProducts(category) {
-  const myPromise = new Promise((resolve, reject) => {
-    const productsList = [
-      {
-        id: 1,
-        title: "Creatina Star Nutrition",
-        price: "2500",
-        stock: "2",
-        category: "creatina",
-        imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/306/768/products/creatina-1kg1-58d5c3eee9f93629aa16173039257327-1024-1024.png",
-      },
-      {
-        id: 2,
-        title: "Creatina Universal",
-        price: "1500",
-        stock: "2",
-        category: "creatina",
-        imageUrl:
-          "https://desuplementos.com/wp-content/uploads/2020/09/Universal-Creatina-500-Grs-Frente-504x504.jpg",
-      },
-      {
-        id: 3,
-        title: "Creatina MuscleTech",
-        price: "1000",
-        stock: "2",
-        category: "creatina",
-        imageUrl:
-          "https://desuplementos.com/wp-content/uploads/2019/12/MUSCLETECH-PLATINUM-CREATINE-504x504.jpg",
-      },
-      {
-        id: 4,
-        title: "Creatina GenTech",
-        price: "2350",
-        stock: "2",
-        category: "creatina",
-        imageUrl:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/247/962/products/creatina-masticable1-fc98b6c9fd8ccb965f16256732393604-1024-1024.jpg",
-      },
-      {
-        id: 5,
-        title: "Proteina ENA True Made",
-        price: "3100",
-        stock: "4",
-        category: "proteina",
-        imageUrl:
-          "https://farmacityar.vteximg.com.br/arquivos/ids/216642-1000-1000/203644_suplemento-dietario-ena-whey-protein-sabor-chocolate-x-1000-g_imagen-1.jpg?v=637705117727900000",
-      },
-      {
-        id: 6,
-        title: "Pre-Entreno PRE-WORKOUT",
-        price: "2765",
-        stock: "1",
-        category: "preentreno",
-        imageUrl:
-          "https://http2.mlstatic.com/D_NQ_NP_886557-MLA49357655448_032022-V.jpg",
-      },
-    ];
-    const productsFiltered = category ? productsList.filter(p => p.category === category) : productsList;
-    setTimeout(() => {
-      resolve(productsFiltered);
-    }, 2000);
-  });
-  return myPromise;
+  const db = getFirestore();
+
+  const itemCollection = collection(db, 'items');
+
+  const q = query(
+    itemCollection
+  );
+
+  return getDocs(q)
 }
 
 function ItemListContainer({ greeting }) {
@@ -73,9 +20,30 @@ function ItemListContainer({ greeting }) {
   const { categoryId } = useParams();
 
   useEffect(() => {
+    // const db = getFirestore();
+
+    // const itemCollection = collection(db, 'items');
+
+    // const q = query(
+    //   itemCollection,
+    //   where('price', '<', 500),
+    //   limit(1)
+    // );
+
+    // getDocs(q)
+    //   .then(snapshot => {
+    //     console.log(snapshot.docs.map(doc => {
+    //       return { ...doc.data(), id: doc.id }
+    //     }));
+    //   })
+    //   .catch(
+    //     err => console.log(err)
+    //   );
     getProducts(categoryId)
-      .then((res) => {
-        setProducts(res);
+    .then(snapshot => {
+      setProducts(snapshot.docs.map(doc => {
+        return { ...doc.data(), id: doc.id }
+      }));
       });
   }, [categoryId]);
 
